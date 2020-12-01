707ad9f02e47a49e03327f3b7c0b0feb70f02d0a
snaggyZeder <nidrit2005@gmail.com>
2020-12-01 18:16:02 +0300

    2st commit -add new files

--- main.cpp
+++ main.cpp
@@ -17,11 +17,40 @@ public:
 	int getnom() {	return nom;	}
 	void setdeNom(int fdenom) { denom = fdenom; }
 	int getdenom() { return denom; }
-	void print() {	cout << nom << '/' << denom << endl;}
+	void print() {
+		cout << nom << '/' << denom << endl;
+		if (nom==0) {
+			cout << '0' << endl;
+		}
+		else
+		if (denom == 0) {
+			cout << "error" << endl;
+		}
+		else
+		cout << nom << '/' << denom << endl;
+	}
 	Fraction mul(Fraction b) {
 		Fraction c;
 		c.nom = nom * b.nom;
 		c.denom = denom * b.denom;
+		return c;
+	}
+	Fraction div(Fraction b) {
+		Fraction c;
+		c.nom = nom* b.denom;
+		c.denom = denom * b.nom;
+		return c;
+	}
+	Fraction add(Fraction b) {
+		Fraction c;
+		c.denom = denom * b.denom;
+		c.nom = nom * b.denom + b.nom * denom;
+		return c;
+	}
+	Fraction sub(Fraction b) {
+		Fraction c;
+		c.denom = denom * b.denom;
+		c.nom = nom * b.denom - b.nom * denom;
 		return c;
 	}
 };
@@ -33,6 +62,12 @@ int main(){
 	a.setdeNom(2);
 	a.print();
 	res = a.mul(b);
+	res.print();
+
+	b.setNom(1);
+	b.setdeNom(3);
+	res = a.div(b);
+	res.print();
 	return 0;
 }
 
